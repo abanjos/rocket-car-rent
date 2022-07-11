889ddb404100d1907a57b33573e336c0ca2efbc7
abanjos <andrebrandg@gmail.com>
2022-07-11 14:51:43 -0300

    Services dir added

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,29 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+/*
+ * [] - Define return type
+ * [] - Define error return
+ * [] - Acess the repository
+ * [] - Return something
+ */
+
+class CreateCategoryService {
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    execute({ name, description }: IRequest): void {
+        const categoryExist = this.categoriesRepository.findByName(name);
+
+        if (categoryExist) {
+            throw new Error("Category Already exist!");
+        }
+
+        this.categoriesRepository.create({ name, description });
+    }
+}
+
+export { CreateCategoryService };
