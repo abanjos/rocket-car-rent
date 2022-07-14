c0f5af1a39c66988e33f365b973f222eb334a080
abanjos <andrebrandg@gmail.com>
2022-07-14 19:11:01 -0300

    Add DB and refactoring repositories

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -15,8 +15,8 @@ interface IRequest {
 class CreateCategoryUseCase {
     constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    execute({ name, description }: IRequest): void {
-        const categoryExist = this.categoriesRepository.findByName(name);
+    async execute({ name, description }: IRequest): Promise<void> {
+        const categoryExist = await this.categoriesRepository.findByName(name);
 
         if (categoryExist) {
             throw new Error("Category Already exist!");
--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
-const categoryRepositories = CategoriesRepository.getInstance();
+const categoryRepositories = null;
 
 const listCategoriesUseCase = new ListCategoriesUseCase(categoryRepositories);
 
