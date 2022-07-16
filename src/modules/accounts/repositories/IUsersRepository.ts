224368a4d7827d215156537730baad4bd61fd433
abanjos <andrebrandg@gmail.com>
2022-07-16 12:57:12 -0300

    added auth verify middleware

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -5,6 +5,7 @@ interface IUsersRepository {
     // receive info from form (need DTO)
     create(data: ICreateUserDTO): Promise<void>;
     findByEmail(email: string): Promise<User>;
+    findById(id: string): Promise<User>;
 }
 
 export { IUsersRepository };
